d9a67a01520cba92c5dbaf6952665f27c3d1388c
浩 <inf2inf2@outlook.com>
2022-12-29 16:16:01 +0800

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
   root "home#index"
 
   get "/admin", to: "admin#index"
-  get "/admin/article", to: "admin#get_article"
-  post "/admin/article", to: "admin#create_article"
-  post "/admin/article/create", to: "admin#create_article"
-  get "/admin/articles/:id/edit", to: "admin#get_edit_article"
-  post "/admin/articles/:id", to: "admin#edit_article"
-  post "/admin/articles/:id/edit", to: "admin#edit_article"
+
+  namespace :admin do
+    resources :articles
+  end
+  #
+  # get "/admin/article", to: "admin#get_article"
+  # post "/admin/article", to: "admin#create_article"
+  # post "/admin/article/create", to: "admin#create_article"
+  # get "/admin/articles/:id/edit", to: "admin#get_edit_article"
+  # post "/admin/articles/:id", to: "admin#edit_article"
+  # post "/admin/articles/:id/edit", to: "admin#edit_article"
+  # delete "/admin/articles/:id/delete", to: "admin#delete_article"
+  # delete "/admin/articles/:id", to: "admin#delete_article"
 
   get "/admin/category", to: "admin#get_category"
 end
