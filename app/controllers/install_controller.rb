1b4256318315af149cf50ed29308cd90d5772698
浩 <inf2inf2@outlook.com>
2022-12-29 17:24:08 +0800

    install routes

--- app/controllers/install_controller.rb
+++ app/controllers/install_controller.rb
@@ -1,2 +1,34 @@
+require 'bcrypt'
+
 class InstallController < ApplicationController
+
+  layout  "install"
+  before_action :check_installed
+  skip_before_action :require_install
+
+  def index
+    render "install/index"
+  end
+
+  def install
+    Option.transaction do
+      Option.create(key: "blog_name", value: params[:blog_name])
+      Option.create(key: "admin_username", value: params[:admin_username])
+      Option.create(key: "admin_password", value: BCrypt::Password.create(params[:admin_password]))
+      Option.create(key: "goose_installed_at", value: Time.now.to_s)
+      $is_goose_installed = true
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def check_installed
+    redirect_to root_path if $is_goose_installed
+  end
+
+  def install_params
+    params.require(:install).permit(:blog_name, :admin_username, :admin_password)
+  end
+
 end
