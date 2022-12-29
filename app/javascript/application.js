51120f060c91718d07b48b32df479693b26fe37b
浩 <inf2inf2@outlook.com>
2022-12-29 11:05:12 +0800

    ignore idea

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import * as bootstrap from "bootstrap"
 import Alpine from 'alpinejs'
 
 
