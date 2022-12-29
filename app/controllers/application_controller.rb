841f9cff3a612cc5951ee6c73868492357962e01
浩 <inf2inf2@outlook.com>
2022-12-29 18:08:45 +0800

    move install flag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :require_install
 
   def require_install
+    if defined?($is_goose_installed) == nil
+      $is_goose_installed = Option.exists?(key: "goose_installed_at")
+    end
     redirect_to install_path unless $is_goose_installed
   end
 
