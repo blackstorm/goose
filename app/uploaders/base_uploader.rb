8b83a59cb7e382e4f26f3a752d3520fa152d0ae4
浩 <inf2inf2@outlook.com>
2023-01-04 13:23:22 +0800

    fix deepsource ruby issue

--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 class BaseUploader < CarrierWave::Uploader::Base
-  @@store_at = "#{ENV["GOOSE_DATA_PATH"]}/uploads"
 
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
   def store_dir
-    @@store_at
+    "#{ENV["GOOSE_DATA_PATH"]}/uploads"
   end
 
 end
