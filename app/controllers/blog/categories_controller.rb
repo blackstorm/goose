4481e2ca6b9efb4126e75164d3c9f4e109c453a6
浩 <inf2inf2@outlook.com>
2023-01-12 22:01:48 +0800

    Fix the missing status field of query articles by category (#27)

--- app/controllers/blog/categories_controller.rb
+++ app/controllers/blog/categories_controller.rb
@@ -6,7 +6,7 @@ module Blog
     def show
       @category = Category.find(params[:id])
       @categories = Category.all
-      @articles = Article.where(category_id: @category.id).order(id: :desc).all
+      @articles = Article.where(category_id: @category.id, status: "published").order(id: :desc).all
       set_page_title @category.name
       render "blog/category"
     end
